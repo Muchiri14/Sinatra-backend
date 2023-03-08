adce5740bcc2b0e3f28a0b6f7fa867b7ae310e52
Daniel <muchiridaniel59@gmail.com>
2023-03-08 06:53:58 +0300

    Code

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+   configure do
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, SecureRandom.hex(24)
+  end
+
+end
---
+++ app/models/shopping_category.rb
@@ -0,0 +1,5 @@
+class ShoppingCategory < ActiveRecord::Base
+    has_many :shopping_items
+    has_many :users, through: :shopping_items
+end
+
